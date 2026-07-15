68f5925eaec3ef3b4d0f65128e66da24ea388643
saniya syed <saniyasyed610@gmail.com>
2026-07-15 16:01:19 +0530

    Delete hello.py

--- hello.py
+++
@@ -1,8 +0,0 @@
-print("===================================")
-print(" IoT Data Analyst Internship ")
-print(" Week 1")
-print("===================================")
-
-print("Hello IoT!")
-print("Welcome to Week 1 of the Internship.")
-print("Learning IoT Fundamentals.")
